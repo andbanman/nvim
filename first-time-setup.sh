a55ec3550fb320c4d04ff084442202fec3dd2464
Andrew Banman <andbanman@users.noreply.github.com>
2022-07-21 14:57:45 -0700

    Check neovim version

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -5,6 +5,11 @@ verlte() {
 	printf '%s\n%s' "${1#[Vv]}" "${2#[Vv]}" | sort -C -V
 }
 
+if ! which nvim &>/dev/null || verlte $(nvim --version | head -n1 | cut -d" " -f2) 0.5; then
+	echo "ERR: nvim version 0.5 or later not installed. Cannot continue."
+	exit 1
+fi
+
 if ! which node &>/dev/null || verlte $(node --version) 12.10.0; then
 	echo "ERR: node version 12.10 or later not installed. Some plugins will not work."
 	echo"       Visit https://nodejs.org/en/download/"
