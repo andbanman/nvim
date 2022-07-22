bce6123eff934b815069857ef1bf0b3be1d23d12
Andrew Banman <andbanman@users.noreply.github.com>
2022-07-22 12:09:00 -0700

    Open NvimTree if in a git repo

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
 require('plugins')
 require("nvim-tree").setup()
 vim.cmd("colorscheme nightfox")
+
+-- Git --
+vim.cmd([[
+  function! TreeIfGitRepo()
+    if isdirectory('./.git')
+      NvimTreeOpen
+    end
+  endfunction
+  augroup git_repository
+    autocmd!
+    autocmd BufEnter * call TreeIfGitRepo()
+  augroup end
+]])
