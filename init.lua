6b451d31f0c3194d3526a4777dbf6c1d2e1c2f60
Andrew Banman <andbanman@users.noreply.github.com>
2022-07-22 13:32:06 -0700

    Keybindings and bash aliases

--- init.lua
+++ init.lua
@@ -2,6 +2,13 @@ require('plugins')
 require("nvim-tree").setup()
 vim.cmd("colorscheme nightfox")
 
+-- Bash --
+vim.cmd("set shellcmdflag=-ic")
+
+-- NVim-Tree --
+vim.api.nvim_set_keymap('n', 'tt', ':NvimTreeToggle<CR>', {})
+vim.api.nvim_set_keymap('n', 'tr', ':NvimTreeRefresh<CR>', {})
+
 -- Git --
 vim.cmd([[
   function! TreeIfGitRepo()
